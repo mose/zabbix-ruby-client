f7753784189cedd43fc6a96a29bc58917cd3d025
mose <mose@mose.com>
2013-09-18 16:10:06 +0800

    fix on init

--- lib/zabbix-ruby-client.rb
+++ lib/zabbix-ruby-client.rb
@@ -1,4 +1,20 @@
 require "zabbix-ruby-client/version"
+require "yaml"
 
 module ZabbixRubyClient
+  extend self
+
+  def run
+    begin
+      @config = YAML::load_file(CONFIG_FILE)
+    rescue Exception => e
+      puts "Configuration file cannot be read"
+      puts e.msg
+      return
+    end
+    puts "running"
+  end
+
+
+
 end
--- lib/zabbix-ruby-client/cli.rb
+++ lib/zabbix-ruby-client/cli.rb
@@ -1,22 +1,38 @@
 require "thor"
+require "zabbix-ruby-client"
 require "zabbix-ruby-client/version"
 
 module ZabbixRubyClient
 
   class Cli < Thor
     include Thor::Actions
+
+    def self.source_root
+      File.expand_path("../../../templates", __FILE__)
+    end
+
+    default_task :help
+
+    desc "init", "Initialize a new zabbix ruby client"
+    def init(name = "zabbix-ruby-client")
+      directory "client", name
+    end
+
+    desc "start", "Collect data according to configuration"
+    option :config,
+      aliases: "-c",
+      banner: "PATH",
+      default: File.expand_path("config.yml", Dir.pwd),
+      desc: "Path to the configuration file to use"
+    def start
+      begin
+        Bundler.require
+      rescue Bundler::GemfileNotFound
+        say "No Gemfile found", :red
+        abort
+      end
+      ZabbixRubyClient.run
+    end
   end
 
-  default_task :help
-
-  desc "init", "Initialize a new zabbix ruby client"
-  def init(name = "zabbix-ruby-client")
-    directory "client", name
-  end
-
-  desc "run", "Collect data according to configuration"
-  def run
-  end
-
-  
 end
