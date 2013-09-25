716cb635d1a52710d514fbfc11c22dc8f0f54753
mose <mose@mose.com>
2013-09-25 18:13:20 +0800

    count bytes not packets, you dumb

--- lib/zabbix-ruby-client/plugins/network.rb
+++ lib/zabbix-ruby-client/plugins/network.rb
@@ -7,8 +7,9 @@ class ZabbixRubyClient
         host = args[0]
         interface = args[1]
         netinfo = `netstat -i | grep "^#{interface} "`
+        metinfo = `grep "#{interface}: " /proc/net/dev`
         if $?.to_i == 0
-          _, mtu, met, rx_ok, rx_err, rx_drop, rx_over, tx_ok, tx_err, tx_drop, tx_over, flags  = netinfo.split(/\s+/)
+          _, rx_ok, rx_packets, rx_err, rx_drop, _, _, _, _, tx_ok, tx_packets, tx_err, tx_drop, _, _, _, _  = netinfo.split(/\s+/)
         else
           logger.warn "Please install netstat."
           return []
@@ -16,16 +17,14 @@ class ZabbixRubyClient
 
         time = Time.now.to_i
         back = []
-        back << "#{host} net.mtu[#{interface}] #{time} #{mtu}"
-        back << "#{host} net.met[#{interface}] #{time} #{met}"
         back << "#{host} net.rx_ok[#{interface}] #{time} #{rx_ok}"
+        back << "#{host} net.rx_packets[#{interface}] #{time} #{rx_packets}"
         back << "#{host} net.rx_err[#{interface}] #{time} #{rx_err}"
         back << "#{host} net.rx_drop[#{interface}] #{time} #{rx_drop}"
-        back << "#{host} net.rx_over[#{interface}] #{time} #{rx_over}"
         back << "#{host} net.tx_ok[#{interface}] #{time} #{tx_ok}"
+        back << "#{host} net.tx_packets[#{interface}] #{time} #{tx_packets}"
         back << "#{host} net.tx_err[#{interface}] #{time} #{tx_err}"
         back << "#{host} net.tx_drop[#{interface}] #{time} #{tx_drop}"
-        back << "#{host} net.tx_over[#{interface}] #{time} #{tx_over}"
         return back
 
       end
