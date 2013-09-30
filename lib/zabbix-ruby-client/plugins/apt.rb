806c81e4e597176d91cd3bd9e45130e6257ff799
mose <mose@mose.com>
2013-09-30 12:47:37 +0800

    adding an apt plugin

---
+++ lib/zabbix-ruby-client/plugins/apt.rb
@@ -0,0 +1,23 @@
+class ZabbixRubyClient
+  module Plugins
+    module Apt
+      extend self
+
+      def collect(*args)
+        aptcheck = `/usr/lib/update-notifier/apt-check`
+        if $?.to_i == 0
+          security, pending = aptcheck.split(/;/).map(&:to_i)
+        else
+          logger.warn "Are you running on ubuntu ?"
+          return []
+        end
+        time = Time.now.to_i
+        back = []
+        back << "#{host} apt[security] #{time} #{security}"
+        back << "#{host} apt[pending] #{time} #{pending}"
+        return back
+      end
+
+    end
+  end
+end
