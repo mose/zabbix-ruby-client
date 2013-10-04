8abbf21d6d155c3ed902a09ff9e48c42c9b7381d
mose <mose@mose.com>
2013-10-04 20:27:06 +0800

    adding a who plugin

---
+++ lib/zabbix-ruby-client/plugins/who.rb
@@ -0,0 +1,33 @@
+class ZabbixRubyClient
+  module Plugins
+    module Who
+      extend self
+
+      def collect(*args)
+        host = args[0]
+        who = get_who
+        if $?.to_i != 0
+          logger.warn "Are you running on ubuntu ?"
+          return []
+        end
+        time = Time.now.to_i
+        back = []
+        back << "#{host} who[total] #{time} #{who}"
+        return back
+      end
+
+      def get_who
+        who = `who`
+        ret = 0
+        who.each_line do |l|
+          ret++
+        end
+        ret
+      end
+
+    end
+  end
+end
+
+ZabbixRubyClient::Plugins.register('who', ZabbixRubyClient::Plugins::Who)
+
