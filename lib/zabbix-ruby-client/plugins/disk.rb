f10c1e19c0839f501811d891070d40d19d8b7538
mose <mose@mose.com>
2013-10-05 17:18:26 +0800

    fix disk mapping

--- lib/zabbix-ruby-client/plugins/disk.rb
+++ lib/zabbix-ruby-client/plugins/disk.rb
@@ -20,10 +20,10 @@ class ZabbixRubyClient
         end
         time = Time.now.to_i
         back = []
-        back << "#{host} disk.space[#{dev},size] #{time} #{size.to_i * 1000}"
-        back << "#{host} disk.space[#{dev},used] #{time} #{used.to_i * 1000}"
-        back << "#{host} disk.space[#{dev},available] #{time} #{available.to_i * 1000}"
-        back << "#{host} disk.space[#{dev},percent_used] #{time} #{percent_used.gsub(/%/,'')}"
+        back << "#{host} disk.space[#{mapped},size] #{time} #{size.to_i * 1000}"
+        back << "#{host} disk.space[#{mapped},used] #{time} #{used.to_i * 1000}"
+        back << "#{host} disk.space[#{mapped},available] #{time} #{available.to_i * 1000}"
+        back << "#{host} disk.space[#{mapped},percent_used] #{time} #{percent_used.gsub(/%/,'')}"
 
         if dev != ""
           diskinfo = `cat /proc/diskstats | grep " #{dev} "`
@@ -33,16 +33,16 @@ class ZabbixRubyClient
             logger.warn "Oh there is no such device."
             return []
           end
-          back << "#{host} disk.io[#{dev},read_ok] #{time} #{read_ok}"
-          back << "#{host} disk.io[#{dev},read_merged] #{time} #{read_merged}"
-          back << "#{host} disk.io[#{dev},read_sector] #{time} #{read_sector}"
-          back << "#{host} disk.io[#{dev},read_time] #{time} #{read_time}"
-          back << "#{host} disk.io[#{dev},write_ok] #{time} #{write_ok}"
-          back << "#{host} disk.io[#{dev},write_merged] #{time} #{write_merged}"
-          back << "#{host} disk.io[#{dev},write_sector] #{time} #{write_sector}"
-          back << "#{host} disk.io[#{dev},write_time] #{time} #{write_time}"
-          back << "#{host} disk.io[#{dev},io_time] #{time} #{io_time}"
-          back << "#{host} disk.io[#{dev},io_weighted] #{time} #{io_weighted}"
+          back << "#{host} disk.io[#{mapped},read_ok] #{time} #{read_ok}"
+          back << "#{host} disk.io[#{mapped},read_merged] #{time} #{read_merged}"
+          back << "#{host} disk.io[#{mapped},read_sector] #{time} #{read_sector}"
+          back << "#{host} disk.io[#{mapped},read_time] #{time} #{read_time}"
+          back << "#{host} disk.io[#{mapped},write_ok] #{time} #{write_ok}"
+          back << "#{host} disk.io[#{mapped},write_merged] #{time} #{write_merged}"
+          back << "#{host} disk.io[#{mapped},write_sector] #{time} #{write_sector}"
+          back << "#{host} disk.io[#{mapped},write_time] #{time} #{write_time}"
+          back << "#{host} disk.io[#{mapped},io_time] #{time} #{io_time}"
+          back << "#{host} disk.io[#{mapped},io_weighted] #{time} #{io_weighted}"
         end
         return back
       end
